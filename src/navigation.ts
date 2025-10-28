17465c530c10d9be55c151a977aff20b2179c6f9
José Pinheiro <jose.pinheiro@cajuinasaogeraldo.com.br>
2025-10-28 08:42:53 -0300

    header

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,104 +7,26 @@ export const headerData = {
       href: '/',
     },
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
+      text: 'Sobre',
+      href: '/sobre',
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+      text: 'Produtos',
+      href: '/produtos',
     },
     {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink(
-            'get-started-website-with-astro-tailwind-css',
-            'post'
-          ),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      text: 'Notícias',
+      href: 'noticias',
     },
     {
-      text: 'Widgets',
+      text: 'Nos Encontre',
       href: '#',
     },
   ],
   actions: [
     {
-      text: 'Download',
-      href: 'https://github.com/arthelokyo/astrowind',
+      text: 'Trabalhe Conosco',
+      href: '#',
       target: '_blank',
     },
   ],
@@ -113,62 +35,73 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Institucional',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Sobre', href: '#' },
+        { text: 'Notícias', href: '#' },
+        { text: 'Trabalhe Conosco', href: '#' },
+        { text: 'Produtos', href: '#' },
+        { text: 'Nos encontre', href: '#' },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Contato',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'Solicite', href: '#' },
+        { text: 'Seja distribuidor', href: '#' },
+        { text: 'Portal de boletos', href: '#' },
+        { text: 'Canal de denúncia', href: '#' },
       ],
     },
     {
-      title: 'Support',
+      title: 'Informações',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Política Anticorrupção', href: '#' },
+        { text: 'Política recebimento de brinde', href: '#' },
+        { text: 'Código de Conduta', href: '#' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Promoções', href: '/promocoes' },
+    { text: 'Política de Privacidade', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    {
+      ariaLabel: 'Facebook',
+      icon: 'mdi:facebook',
+      href: 'https://www.facebook.com/cajuinasaogeraldo',
+    },
+    {
+      ariaLabel: 'X',
+      icon: 'tabler:brand-x',
+      href: 'https://x.com/saogeraldocaju',
+    },
+    {
+      ariaLabel: 'Instagram',
+      icon: 'mdi:instagram',
+      href: 'https://www.instagram.com/cajuinasaogeraldo/#',
+    },
+    {
+      ariaLabel: 'Youtube',
+      icon: 'mdi:youtube',
+      href: 'https://www.youtube.com/c/cajuinasaogeraldo',
+    },
+    {
+      ariaLabel: 'Linkedin',
+      icon: 'mdi:linkedin',
+      href: 'https://www.linkedin.com/company/cajuinasaogeraldo',
+    },
+    {
+      ariaLabel: 'Tiktok',
+      icon: 'tabler:brand-tiktok',
+      href: 'https://www.tiktok.com/@saogeraldocaju',
+    },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    <span>&copy; 2025 Todos os direitos reservados à Cajuína São Geraldo</span>
+    <span class="hidden md:inline">•</span>
+    <span>Av. Padre Cícero, Km 02 — Juazeiro do Norte, CE — CEP: 63022-115</span>
   `,
 };
