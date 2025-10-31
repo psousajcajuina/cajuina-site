2b80f95572b07c2c7156805c0fa4e90ec2198f29
José Pinheiro <jose.pinheiro@cajuinasaogeraldo.com.br>
2025-10-31 15:51:27 -0300

    feat: adicionar novo mapa e atualizar a galeria de fotos sobre a apresentação

--- src/components/react/AboutPhotoGallery.tsx
+++ src/components/react/AboutPhotoGallery.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// import 'swiper/css';
-// import 'swiper/css/effect-coverflow';
-// import 'swiper/css/pagination';
-
 import { Autoplay, FreeMode, Navigation } from 'swiper/modules';
 
 const items = Array.from({ length: 10 }).map((_, i) => ({
@@ -13,39 +9,28 @@ const items = Array.from({ length: 10 }).map((_, i) => ({
 
 export default function AboutPhotoGallery() {
   return (
-    <div className="mx-auto mt-5 mb-5 w-full max-w-6xl px-2">
-      <Swiper
-        modules={[Navigation, Autoplay, FreeMode]}
-        slidesPerView={'auto'}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: true,
-        }}
-        grabCursor
-      >
-        {items.map((item, index) => (
-          <SwiperSlide className="size-[135px]!" key={item.url + index}>
-            <img src={item.url} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="relative w-full overflow-hidden">
+      <div className="-mx-[calc(100vw-100%)] my-5 w-screen">
+        <Swiper
+          modules={[Navigation, Autoplay, FreeMode]}
+          slidesPerView={3}
+          spaceBetween={12}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: true,
+          }}
+          grabCursor
+        >
+          {items.map((item, index) => (
+            <SwiperSlide key={item.url + index}>
+              <img
+                className="size-[135px] rounded-[28px] object-cover"
+                src={item.url}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
-// export default function AboutPhotoGallery() {
-//   console.log('AboutPhotoGallery CHAMADO!');
-
-//   return (
-//     <div
-//       style={{
-//         background: 'green',
-//         color: 'white',
-//         padding: '100px',
-//         fontSize: '40px',
-//         border: '5px solid red',
-//       }}
-//     >
-//       🎉 COMPONENTE REACT FUNCIONANDO! 🎉
-//     </div>
-//   );
-// }
