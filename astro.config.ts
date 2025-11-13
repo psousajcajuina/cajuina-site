0c8ac17f3a30cc3ca3684ef33d139a4046ee7350
José Pinheiro <jose.pinheiro@cajuinasaogeraldo.com.br>
2025-11-13 17:32:16 -0300

    feat: adiciona 'astro-seo-schema' ao forward de partytown

--- astro.config.ts
+++ astro.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
     }),
     ...whenExternalScripts(() =>
       partytown({
-        config: { forward: ['dataLayer.push'] },
+        config: { forward: ['dataLayer.push', 'astro-seo-schema'] },
       })
     ),
     compress({
